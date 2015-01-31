429675897e0b722eaf33c6e59454f52b13708103
Jason Nordwick <jnordwick@gmail.com>
2015-01-30 22:51:35 -0500

    Ripped code from zerog library proper. Initial check in.

--- src/zerog/util/grisu/Grisu.java
+++ src/zerog/util/grisu/Grisu.java
@@ -1,5 +1,388 @@
 package zerog.util.grisu;
 
+import static zerog.util.grisu.DiyFp.doubleExponentBias;
+import static zerog.util.grisu.DiyFp.doubleMantissaSize;
+import static zerog.util.grisu.DiyFp.u_doubleExponentMask;
+import static zerog.util.grisu.DiyFp.u_doubleHiddenBit;
+import static zerog.util.grisu.DiyFp.u_doubleMantissaMask;
+
+// TODO: add rounding to number of decimals
 public class Grisu {
+
+    public static final Grisu format = new Grisu( 16, 10, 'e' );
+
+    public final int maxintdigits;
+    public int maxfracdigits;
+    public byte exp_char;
+
+    static final int max_grisu_precision = 17;
+
+    // as long as `max_int_digits` is less than `max_grisu_precisio` this holds
+    // +1 for minus, +1 for dot, +5 for exponent (+/-, e, 3 digits).
+    public static final int longest_double_output = max_grisu_precision + 1 + 1 + 5;
+
+    static final byte[] nan_text = "NaN".getBytes();
+    static final byte[] inf_text = "Infinity".getBytes();
+    static final byte[] zero_text = "0.0".getBytes();
+
+    public Grisu( int max_int_digits, int max_frac_digits, char exp_char ) {
+
+        this.maxintdigits = max_int_digits;
+        this.maxfracdigits = max_frac_digits;
+        this.exp_char = (byte)exp_char;
+    }
+
+    public String doubleToString( double value ) {
+
+        byte[] buf = new byte[ longest_double_output ];
+        int len = doubleToBytes( buf, 0, value );
+        return new String( buf, 0, len );
+    }
+
+    public int doubleToBytes( byte[] buffer, int boffset, double value ) {
+
+        // Get the special cases out of the way: NaN, infinities, zero(s)
+        if( Double.isNaN( value )) {
+            // D.isNan() hopefully catches all the NaN values, not just the canonical
+            System.arraycopy( nan_text, 0, buffer, boffset, nan_text.length );
+            return nan_text.length;
+        }
+        else if( value == Double.POSITIVE_INFINITY ) {
+
+            System.arraycopy( inf_text, 0, buffer, boffset, inf_text.length );
+            return inf_text.length;
+        }
+        else if( value == Double.NEGATIVE_INFINITY ) {
+
+            buffer[boffset] = '-';
+            System.arraycopy( inf_text, 0, buffer, boffset + 1, inf_text.length );
+            return 1 + inf_text.length;
+        }
+        else if( value == 0 ) {
+            // NOTE: Should I test for +/-0.0 separately?
+            System.arraycopy( zero_text, 0, buffer, boffset, zero_text.length );
+            return zero_text.length;
+        }
+
+        // So we have a number to stringify now
+        int pos = 0;
+        if( value < 0 ) {
+            buffer[boffset + pos] = '-';
+            pos += 1;
+            value = -value;
+        }
+
+        long u_lenpow = u_grisu2( buffer, boffset + pos, value );
+        return pos + formatBuffer( buffer, boffset + pos, StuffedPair.car( u_lenpow ), StuffedPair.cdr( u_lenpow ));
+    }
+
+
+    private static void round(byte[] buffer, int pos, long u_delta, long u_rest, long  u_onef, long u_winf) {
+
+        while (Long.compareUnsigned(u_rest, u_winf) < 0
+                && Long.compareUnsigned(u_delta - u_rest, u_onef) >= 0
+                && (Long.compareUnsigned(u_rest + u_onef, u_winf) < 0
+                        || Long.compareUnsigned(u_winf - u_rest, u_rest + u_onef - u_winf) > 0)) {
+
+            buffer[pos - 1]--;
+            u_rest += u_onef;
+        }
+    }
+
+    // I wonder if a straight for-loop would be quicker because of branch
+    // mispredictions. The loop would probably get unrolled anyways.
+    private static int numUnsignedDigits( int u_x ) {
+        int n = 1;
+
+        if (u_x >= 100_000_000) {
+            u_x /= 100_000_000;
+            n += 8;
+        }
+        if (u_x >= 10_000) {
+            u_x /= 10_000;
+            n += 4;
+        }
+        if (u_x >= 100) {
+            u_x /= 100;
+            n += 2;
+        }
+        if (u_x >= 10) {
+            u_x /= 10;
+            n += 1;
+        }
+
+        return n;
+    }
+
+    private static long u_digitGen( long u_vf, int ve, long u_pf, int pe, long u_delta, byte[] buffer, int boffset, int base10exp ) {
+
+        long u_onef = 1L << -pe;
+        long u_fracMask = u_onef - 1;
+        long u_winf = u_pf - u_vf;
+
+        // Grab the integral and fractional parts.
+        int u_intpart = (int)(u_pf >>> -pe);
+        long u_fracpart = u_pf & u_fracMask;
+
+        int digits = numUnsignedDigits( u_intpart );
+        int pos = boffset;
+
+        // Write the integer part.
+        while( digits > 0 ) {
+
+            int pow10 = (int)CachedPowers.u_pow10[digits - 1];
+            int u_dig = Integer.divideUnsigned( u_intpart, pow10 );
+            u_intpart = Integer.remainderUnsigned( u_intpart, pow10 );
+
+            // no leading zeros
+            if( !(pos == boffset && u_dig == 0) )
+                buffer[pos++] = (byte)('0' + u_dig);
+
+            long u_more = (Integer.toUnsignedLong( u_intpart ) << -pe) + u_fracpart;
+            digits--;
+
+            // No use going any further, so truncate it off and round.
+            if (Long.compareUnsigned( u_more, u_delta ) <= 0) {
+
+                base10exp += digits;
+                round( buffer, pos, u_delta, u_more, CachedPowers.u_pow10[digits] << -pe, u_winf );
+                return StuffedPair.cons( pos - boffset, base10exp );
+            }
+        }
+
+        // Write the fractional part
+        for (;;) {
+
+            u_fracpart *= 10;
+            u_delta *= 10;
+
+            int u_dig = (int)(u_fracpart >>> -pe);
+
+            // no leading zeros
+            if( !(pos == boffset && u_dig == 0) )
+                buffer[pos++] = (byte)('0' + u_dig);
+
+            u_fracpart &= u_fracMask;
+            digits--;
+
+            // no use going any further. Trunc and round as above.
+            if (Long.compareUnsigned(u_fracpart, u_delta) <= 0) {
+
+                base10exp += digits;
+                round(buffer, pos, u_delta, u_fracpart, u_onef, u_winf * CachedPowers.u_pow10[-digits]);
+                //				System.out.println( "cons=" + StuffedPair.toString(StuffedPair.cons(pos-boffset,base10exp)));
+                return StuffedPair.cons( pos - boffset, base10exp );
+            }
+        }
+    }
+
+    private static long u_grisu2( byte[] buffer, int boffset, double value ) {
+
+        // copy and paste from DiyFp(), I admit it.
+        long u_vbits = Double.doubleToLongBits( value );
+        int ve = (int)(( u_vbits & u_doubleExponentMask ) >>> doubleMantissaSize);
+        long u_vf = u_vbits & u_doubleMantissaMask;
+
+        if( ve != 0 ) {
+            // normalized
+            u_vf |= u_doubleHiddenBit;
+            ve -= doubleExponentBias;
+        }
+        else {
+            // denormalized
+            ve =  1 - doubleExponentBias;
+        }
+
+        // calculate the lower and upper bounds
+        int shiftval = u_vf == u_doubleHiddenBit ? 2 : 1;
+        long u_mf = (u_vf << shiftval) - 1;
+        int me = ve - shiftval;
+
+        long u_pf = (u_vf << 1) + 1;
+        int pe = ve - 1;
+
+        // normalize the three ersatz floats
+        shiftval = Long.numberOfLeadingZeros( u_vf );
+        u_vf <<= shiftval;
+        ve -= shiftval;
+
+        shiftval = Long.numberOfLeadingZeros( u_mf );
+        u_mf <<= shiftval;
+        me -= shiftval;
+
+        shiftval = Long.numberOfLeadingZeros( u_pf );
+        u_pf <<= shiftval;
+        pe -= shiftval;
+
+        // Find the correct cached power of 10 in ersatz float representation
+        int index = CachedPowers.cacheIndexFrom2Exp( ve );
+        int base10exp = CachedPowers.exponentFromIndex( index );
+        long u_powf = CachedPowers.u_f[index];
+        int powe = CachedPowers.e[index];
+
+        // multiple the value and the window by the cached approximation
+        u_vf = DiyFp.u_multiplySignificands( u_powf, u_vf );
+        ve = DiyFp.multiplyExponents( powe, ve );
+
+        u_mf = DiyFp.u_multiplySignificands( u_powf, u_mf );
+        me = DiyFp.multiplyExponents( powe, me );
+
+        u_pf = DiyFp.u_multiplySignificands( u_powf, u_pf );
+        pe = DiyFp.multiplyExponents( powe, pe );
+
+        return u_digitGen(u_vf, ve, u_pf, pe, u_pf - u_mf, buffer, boffset, base10exp);
+    }
+
+    private int appendExponent(byte[] buffer, int boffset, int base10exp ) {
+
+        int pos = boffset;
+
+        buffer[pos++] = exp_char;
+
+        if (base10exp < 0) {
+
+            buffer[pos++] = '-';
+            base10exp = -base10exp;
+        }
+        else {
+
+            buffer[pos++] = '+';
+        }
+
+        if (base10exp >= 100) {
+
+            buffer[pos] = (byte)('0' + (base10exp / 100));
+            base10exp %= 100;
+            buffer[pos + 1] = (byte)('0' + (base10exp / 10));
+            buffer[pos + 2] = (byte)('0' + (base10exp % 10));
+            pos += 3;
+        }
+        else if (base10exp >= 10) {
+
+            buffer[pos] = (byte)('0' + (base10exp / 10));
+            buffer[pos + 1] = (byte)('0' + (base10exp % 10));
+            pos += 2;
+        }
+        else {
+
+            buffer[pos] = (byte)('0' + base10exp);
+            pos += 1;
+        }
+
+        return pos - boffset;
+    }
+
+    // Grisu just gives you a string of numbers and a base 10 exponent.
+    // This makes it easier to read (e.g, 0.1 isn't rendered as 1 * 10^-1)
+    // This is conceptually simple, but intricate in practice, especially when
+    // trying to write into a buffer at a offset.
+    //
+    // This only write positive values. It is assumed the negative sign will
+    // be taken care of elsewhere.
+    //
+    // returns the final length of the formatted number
+    private int formatBuffer( byte[] buffer, int boffset, int blen, int exp ) {
+
+        //		System.out.println("------------------------------------");
+        //		System.out.println( new String(buffer).trim() );
+        //		System.out.println( "boffset=" + boffset );
+        //		System.out.println( "blen=" + blen );
+        //		System.out.println( "exp=" + exp );
+
+        int givendigits = blen;
+
+        // This is going to get ugly. SESE-minded people should avert their eyes.
+        if( exp >= 0 ) {
+
+            int totaldigits = givendigits + exp;
+
+            if( totaldigits <= maxintdigits ) {
+
+                // easiest, just extend zeros, if any, and append zero fraction
+                // 12e0 -> 12.0 and 432e1 -> 4320.0
+                for( ; givendigits < totaldigits; ++givendigits )
+                    buffer[boffset + givendigits] = '0';
+
+                buffer[boffset + totaldigits] = '.';
+                buffer[boffset + totaldigits + 1] = '0';
+
+                return totaldigits + 2;
+            }
+
+            else {
+                // These will all be large numbers:
+                // 123456789e0 -> 1.23456789e8 and 123e10 -> 1.23e12
+                int elen;
+                if( givendigits == 1 ) {
+                    // 1e10 -> 1e10
+                    elen = appendExponent( buffer, boffset + givendigits, exp );
+                }
+                else {
+                    // 123e10 -> 1.23e12
+                    System.arraycopy( buffer, boffset + 1, buffer, boffset + 2, givendigits );
+                    buffer[boffset + 1] = '.';
+                    blen += 1; // now we have a '.' in the number
+                    elen = appendExponent( buffer, boffset + blen, exp + givendigits - 1 );
+                }
+
+                return blen + elen;
+            }
+        }
+        else {
+            // From here on down, we have negative exponents
+
+            int fracdigits = -exp;
+            int intdigits = Math.max( givendigits - fracdigits, 0 );
+            int totaldigits = intdigits + fracdigits;
+
+            //			System.out.println( "intdigits=" + intdigits );
+
+            if( intdigits > 0 && intdigits <= maxintdigits ) {
+
+                // 12345e-2 -> 123.45
+                System.arraycopy( buffer, boffset + intdigits, buffer, boffset + intdigits + 1, fracdigits );
+                buffer[boffset + intdigits] = '.';
+
+                return givendigits + 1; // just inserted a dot
+            }
+
+            else if( totaldigits <= maxfracdigits ) {
+
+                // 12345e-5 -> 0.12345 and 12e-3 -> 0.012
+                int leadingzeros = fracdigits - givendigits;
+                int leadingspace = leadingzeros + 2;
+
+                System.arraycopy( buffer, boffset, buffer, boffset + leadingspace, givendigits );
+
+                buffer[boffset] = '0';
+                buffer[boffset + 1] = '.';
+                for( int i = 0; i < leadingzeros; ++i )
+                    buffer[boffset + 2 + i] = '0';
+
+                return leadingspace + givendigits;
+            }
+            else {
+
+                if( givendigits == 1 ) {
+                    int explen = appendExponent( buffer, boffset + givendigits, exp );
+                    return boffset + givendigits + explen;
+                }
+                else {
+
+                    // These are all exponential form at least `precision` digits long or
+                    // with at least `precision` integer digits.
+                    // 123456789e-2 -> 1.23456789e+6 and 1234e-20 -> 1.234e-17
+                    System.arraycopy( buffer, boffset + 1, buffer, boffset + 2, givendigits - 1 );
+                    buffer[boffset + 1] = '.';
+
+                    int newexp = exp + givendigits - 1;
+                    int explen = appendExponent( buffer, boffset + givendigits + 1, newexp );
+
+                    return 1 + givendigits + explen;
+                }
+            }
+        }
+        // assert false : "Unreachable";
+    }
 
 }
---
+++ test/zerog/util/grisu/GrisuTest.java
@@ -0,0 +1,250 @@
+package zerog.util.grisu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GrisuTest {
+
+    @Test
+    public void test_zero() {
+        double d = 0.0;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals( "0.0", s );
+    }
+
+    @Test
+    public void test_pinf() {
+        double d = 1.0 / 0.0;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals( "Infinity", s );
+    }
+
+    @Test
+    public void test_ninf() {
+        double d = -1.0 / 0.0;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals( "-Infinity", s );
+    }
+
+    @Test
+    public void test_inf() {
+        double d = 0.0 / 0.0;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals( "NaN", s );
+    }
+
+    @Test
+    public void test_long1() {
+        double d = 987654321.123456789;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("987654321.1234568", s);
+    }
+
+    @Test
+    public void test_nlong1() {
+        double d = -987654321.123456789;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("-987654321.1234568", s);
+    }
+
+    @Test
+    public void test_big1() {
+        double d = 123456789.123456789*123123123.123123123*456456456.456456456;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("6.938314078198765e+24", s);
+    }
+
+    @Test
+    public void test_nbig1() {
+        double d = -123456789.123456789*123123123.123123123*456456456.456456456;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("-6.938314078198765e+24", s);
+    }
+
+    @Test
+    public void test_huge1() {
+        double d = 123456789123456789.0 * 1.0e+291;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("1.2345678912345678e+308", s);
+    }
+
+    @Test
+    public void test_nhuge1() {
+        double d = -123456789123456789.0 * 1.0e+291;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("-1.2345678912345678e+308", s);
+    }
+
+    @Test
+    public void test_small1() {
+        double d = 0.00000123456789*0.00000000123123123*0.00000000456456456;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("6.938314057383822e-24", s);
+    }
+
+    @Test
+    public void test_nsmall1() {
+        double d = -0.00000123456789*0.00000000123123123*0.00000000456456456;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("-6.938314057383822e-24", s);
+    }
+
+    @Test
+    public void test_tiny1() {
+        double d = 123456789123456789.0 * 1.0e-323;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("1.2199151650353452e-306", s);
+    }
+
+    @Test
+    public void test_ntiny1() {
+        double d = -123456789123456789.0 * 1.0e-323;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("-1.2199151650353452e-306", s);
+    }
+
+    @Test
+    public void test_x1() {
+        double d = 123.453;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("123.453", s);
+    }
+
+    @Test
+    public void test_x2() {
+        double d = 0.123453;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("0.123453", s);
+    }
+
+    @Test
+    public void test_x3() {
+        double d = 123453.0;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("123453.0", s);
+    }
+
+    @Test
+    public void test_x4() {
+        long n = 5404319552844595L;
+        long d = 18014398509481984L;
+
+        String s = Grisu.format.doubleToString( (double)n / (double)d);
+        assertEquals("0.3", s);
+    }
+
+    // When the variable width formatting is added, add this back in
+    // @Test
+    public void test_x5() {
+        double d = 0.2999999999999999;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("0.2999999999999999", s);
+    }
+
+    @Test
+    public void test_x6() {
+        double d = 0.29999999999999999;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("0.3", s);
+    }
+
+    @Test
+    public void test_max() {
+        double d = 1.7976931348623157e+308; // 0x7FEFFFFFFFFFFFFF
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("1.7976931348623157e+308", s);
+    }
+
+    @Test
+    public void test_nmax() {
+        double d = -1.7976931348623157e+308; // 0xFFEFFFFFFFFFFFFF
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("-1.7976931348623157e+308", s);
+    }
+
+    @Test
+    public void test_min() {
+        double d = 5.0e-324; // 0x1
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("5e-324", s);
+    }
+
+    @Test
+    public void test_border1() {
+        double d = 1.125;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("1.125", s);
+    }
+
+    @Test
+    public void test_border2() {
+        double d = 8.0;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("8.0", s);
+    }
+
+    @Test
+    public void test_border3() {
+        double d = 3.9999999999999997;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("3.9999999999999996", s);
+    }
+
+    @Test
+    public void test_border4() {
+        double d = 1e+308;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("1e+308", s);
+    }
+
+    @Test
+    public void test_border5() {
+        double d = 1e-323;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("1e-323", s);
+    }
+
+    @Test
+    public void test_border6() {
+        double d = 1e+307;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("1e+307", s);
+    }
+
+
+    public void test_border7() {
+        double d = 1e-322;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("1e-322", s);
+    }
+
+
+    @Test
+    public void test_denorm1() {
+        double d = 1.23e-321;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("1.23e-321", s);
+    }
+
+    @Test
+    public void test_denorm2() {
+        double d = 1.2345e-321;
+        String s = Grisu.format.doubleToString( d );
+        assertEquals("1.235e-321", s);
+    }
+
+    @Test
+    public void test_pow10s() {
+
+        for(int i = -323; i <= 308; ++i ) {
+            if( i >= -20 && i <= 20 ) // The format in here changes
+                continue;
+            String sd = "1e" + (i > 0 ? "+" : "") + i;
+            double d = Double.parseDouble( "1e" + i );
+            String s = Grisu.format.doubleToString( d );
+            assertEquals(sd, s);
+        }
+    }
+
+}
