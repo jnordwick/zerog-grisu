eebcc2b0c246a1f695c81d58a47307b0963892dc
Jason Nordwick <jnordwick@gmail.com>
2015-02-01 14:20:08 -0500

    Improved jmh code a little.

--- test/zerog/util/grisu/JmhBenchmark.java
+++ test/zerog/util/grisu/JmhBenchmark.java
@@ -40,33 +40,34 @@ import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 /* 
- * Current JMH bench, similar on small numbers (no fast path
- * code yet) and 75% faster on completely random numbers.
+ * Current JMH bench, similar on small numbers (no fast path code yet)
+ * and 40% faster on completely random numbers.
  * 
  * Benchmark                         Mode  Cnt         Score         Error  Units
- * JmhBenchmark.test_lowp_doubleto  thrpt   40  10337464.294 ± 1515567.984  ops/s
- * JmhBenchmark.test_lowp_grisubuf  thrpt   40  10269674.393 ±  765917.689  ops/s
- * JmhBenchmark.test_lowp_grisustr  thrpt   40   4926455.732 ±  564312.662  ops/s
+ * JmhBenchmark.test_lowp_doubleto  thrpt   20  11439027.798 ± 2677191.952  ops/s
+ * JmhBenchmark.test_lowp_grisubuf  thrpt   20  11540289.271 ±  237842.768  ops/s
+ * JmhBenchmark.test_lowp_grisustr  thrpt   20   5038077.637 ±  754272.267  ops/s
  * 
- * JmhBenchmark.test_rand_doubleto  thrpt   40   1785882.370 ±  103271.284  ops/s
- * JmhBenchmark.test_rand_grisubuf  thrpt   40   3087466.908 ±  400094.707  ops/s
- * JmhBenchmark.test_rand_grisustr  thrpt   40   2170533.061 ±  266207.143  ops/s
+ * JmhBenchmark.test_rand_doubleto  thrpt   20   1841031.602 ±  219147.330  ops/s
+ * JmhBenchmark.test_rand_grisubuf  thrpt   20   2609354.822 ±   57551.153  ops/s
+ * JmhBenchmark.test_rand_grisustr  thrpt   20   2078684.828 ±  298474.218  ops/s
  * 
  * This doens't account for any garbage costs either since the benchmarks
  * aren't generating enough to trigger GC, and Java internally uses per-thread
- * objects to avoid many allcation costs.
+ * objects to avoid some allocations.
  * 
  * Don't call Grisu.doubleToString() except for testing. I think the extra
- * allocations and copying are killing it. I'll fix it.
+ * allocations and copying are killing it. I'll fix that.
  */
 
 public class JmhBenchmark {
     
-    static double[] random_values = new double[1000000];
-    static double[] lowp_values = new double[1000000];
+    static final int nmask = 1024*1024 - 1;
+    static final double[] random_values = new double[nmask + 1];
+    static final double[] lowp_values = new double[nmask + 1];
     
-    static byte[] buffer = new byte[30];
-    static byte[] bresults = new byte[30];
+    static final byte[] buffer = new byte[30];
+    static final byte[] bresults = new byte[30];
     
     static int i = 0;
     static final Grisu g = Grisu.fmt;
@@ -87,43 +88,43 @@ public class JmhBenchmark {
 
     @Benchmark
     public String test_rand_doubleto() {
-        i = (i + 1) % lowp_values.length;
         String s = Double.toString( random_values[i] );
+        i = (i + 1) & nmask;
         return s;
     }
     
     @Benchmark
     public String test_lowp_doubleto() {
-        i = (i + 1) % lowp_values.length;
         String s = Double.toString( lowp_values[i] );
+        i = (i + 1) & nmask;
         return s;
     }
     
     @Benchmark
     public String test_rand_grisustr() {
-        i = (i + 1) % lowp_values.length;
         String s =  g.doubleToString( random_values[i] );
+        i = (i + 1) & nmask;
         return s;
     }
     
     @Benchmark
     public String test_lowp_grisustr() {
-        i = (i + 1) % lowp_values.length;
         String s =  g.doubleToString( lowp_values[i] );
+        i = (i + 1) & nmask;
         return s;
     }
     
     @Benchmark
     public byte[] test_rand_grisubuf() {
-        i = (i + 1) % lowp_values.length;
         g.doubleToBytes( bresults, 0, random_values[i] );
+        i = (i + 1) & nmask;
         return bresults;
     }
     
     @Benchmark
     public byte[] test_lowp_grisubuf() {
-        i = (i + 1) % lowp_values.length;
         g.doubleToBytes( bresults, 0, lowp_values[i] );
+        i = (i + 1) & nmask;
         return bresults;
     }
 
@@ -132,7 +133,7 @@ public class JmhBenchmark {
                 .include(".*" + JmhBenchmark.class.getSimpleName() + ".*")
                 .warmupIterations(20)
                 .measurementIterations(20)
-                .forks(0)
+                .forks(1)
                 .build();
 
         new Runner(opt).run();
